b589f7e43d31aad84f1390def71c85eddd10e3e3
reagan bouesso <rbpelenda@gmail.com>
2020-02-16 00:04:11 +0100

    my first esercise to DI(dependency Injection), in the biginning part

---
+++ MyExecuter/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyExecuter
+{
+    public interface text
+    {
+        void print();
+    }
+    class format : text
+    {
+        public void print()
+        {
+            Console.WriteLine("here the text format");
+        }
+    }
+
+    public class construcroInjection
+    {
+        private text _text;
+
+        public construcroInjection(text t1)
+        {
+            _text = t1;
+        }
+
+        public void print()
+        {
+            _text.print();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
